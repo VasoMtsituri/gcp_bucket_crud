27fdb59e9b5ee697ea2c843d1f67510f90130d8d
Your Name <you@example.com>
2022-03-31 15:09:28 +0400

    add exception handling in create_bucket method

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,3 @@
 GCP_CREDS = '/home/vaso/PycharmProjects' \
             '/gcp_bucket_crud/seismic-hope-345015-c4e38ce9f845.json'
+GCP_LOCATION_EU = 'eu'
--- utils/gcp_bucket.py
+++ utils/gcp_bucket.py
@@ -1,8 +1,13 @@
 import os
+import logging
+from urllib.error import HTTPError
+
 from google.cloud import storage
-from utils.constants import GCP_CREDS
+
+from utils.constants import GCP_CREDS, GCP_LOCATION_EU
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = GCP_CREDS
+logging.basicConfig(level=logging.DEBUG)
 
 
 class CloudStorageBucketClient:
@@ -13,6 +18,11 @@ class CloudStorageBucketClient:
     def storage_client(self):
         return self.__storage_client
 
-    def create_bucket(self, bucket_name, location='eu'):
-        self.storage_client.create_bucket(bucket_name, location=location)
-
+    def create_bucket(self, bucket_name, location=GCP_LOCATION_EU):
+        try:
+            self.storage_client.create_bucket(bucket_name, location=location)
+            logging.debug('Bucket created successfully')
+            return 'OK', 201
+        except HTTPError as http_error:
+            logging.debug(f'HTTPError error occurred: {http_error}')
+            return 'Creating new bucket failed'
