97806b1cb601bd7f05e7ae46334242d47326fc96
Your Name <you@example.com>
2022-03-31 15:37:18 +0400

    add get and list method in CloudStorageBucketClient class

--- utils/gcp_bucket.py
+++ utils/gcp_bucket.py
@@ -21,8 +21,27 @@ class CloudStorageBucketClient:
     def create_bucket(self, bucket_name, location=GCP_LOCATION_EU):
         try:
             self.storage_client.create_bucket(bucket_name, location=location)
-            logging.debug('Bucket created successfully')
+            logging.info('Bucket created successfully')
             return 'OK', 201
         except HTTPError as http_error:
             logging.debug(f'HTTPError error occurred: {http_error}')
             return 'Creating new bucket failed'
+
+    def retrieve_bucket(self, bucket_name):
+        try:
+            bucket = self.storage_client.get_bucket(bucket_name)
+            logging.info('Bucket retrieved successfully')
+            return bucket
+        except HTTPError as http_error:
+            logging.debug(f'HTTPError error occurred: {http_error}')
+            return f'Retrieving bucket with name {bucket_name} failed'
+
+    def retrieve_buckets(self, max_results=None, prefix=None):
+        try:
+            buckets = self.storage_client.list_buckets(max_results=max_results,
+                                                       prefix=prefix)
+            logging.info('Buckets retrieved successfully')
+            return buckets
+        except HTTPError as http_error:
+            logging.debug(f'HTTPError error occurred: {http_error}')
+            return f'Retrieving buckets failed'
